b1834a602b0d1383bb6821615006817f39be29a3
NoaVelasco <noavelasco@gmail.com>
2022-03-19 14:35:09 +0100

    220319 Fibonacci

--- mates.py
+++ mates.py
@@ -9,3 +9,29 @@ def multiplicador(n):
 
 doble = multiplicador(2)
 triple = multiplicador(3)
+
+
+def fibonacci(n):
+    """Imprime la sucesión desde 0 a tantas veces como n."""
+    
+    a = 0
+    b = 1
+
+    if n <= 0:
+        return "Debe ser mayor que 0"
+
+    if n == 1:
+        return 0
+
+    if n == 2:
+        print(0)
+        return 1
+
+    print(0)
+    for i in range(2, n):
+        sum = a+b
+        a = b
+        b = sum
+        print(a)
+    return b
+
