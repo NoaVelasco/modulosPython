4ee06ed140d8979961f3b5c3993f55b3533be035
NoaVelasco <noavelasco@gmail.com>
2022-04-02 23:21:04 +0200

    220402 mejoras listas y diccionarios

---
+++ diccionarios.py
@@ -0,0 +1,14 @@
+# Python 3.10.2 UTF-8
+# Copyright (c) 2022, Noa Velasco
+
+
+def dictiozip(listaClaves, listaValores):
+    """
+    Devuelve un diccionario a partir de dos listas equivalentes
+    en número en las cuales una contiene las claves y la otra los valores.
+    """
+    diccionario = {clave: valor for (
+        clave, valor) in zip(listaClaves, listaValores)}
+    return diccionario
+
+
--- listas.py
+++ listas.py
@@ -7,7 +7,6 @@ from random import randint
 
 def replindex(lista):
     """Str. Cambiar el elemento de una lista mediante su número indexado"""
-    
     print(lista)
 
     orden = int(input("index: "))
@@ -19,7 +18,6 @@ def replindex(lista):
 
 def replname(lista):
     """Str. Cambiar el elemento de una lista mediante su nombre"""
-    
     print(lista)
 
     buscar = input("elemento a buscar: ")
@@ -32,45 +30,43 @@ def replname(lista):
 
 def bingo(lista):
     """Int. ¿Está el número en la lista?"""
-
     buscanum = int(input())
     if buscanum in lista:
-        print("True")
+        print("¡Bingo! :)")
     else:
-        print("False")
+        print("¡Agua! :(")
 
 
 def stats(lista):
     """Imprime varios atributos de una lista"""
-
     print(lista)
     print("La lista contiene", len(lista), "elementos")
     print("Su valor mínimo es", min(lista))
     print("Su valor máximo es", max(lista))
     print("La suma de sus valores es", sum(lista))
-    print("La media de sus valores es", sum(lista)/len(lista))
+    print("La media de sus valores es", sum(lista)/len(lista)) # numpy.mean()
+    print("El mediano de sus valores es", sorted(lista)[len(lista)//2]) # numpy.median()
+    #por hacer: scipy.stats.mode: el valor más repetido
 
 
-def creador(nombre):
+def creador():
     """
     Crea listas [str] mediante inputs.
-    Llamar a la función con input para el nombre.
+    Llamar a la función dentro de una variable que será la lista.
     Luego la función adicion() añade elementos.
     """
-
     nombre = []
     adicion(nombre)
-    print(nombre)
+    return nombre
 
 
 def adicion(lista):
-    """Añade elementos [str] a una lista ya creada. Sale escribiendo '_esc_'."""
-
+    """Añade elementos [str] a una lista ya creada. Sale escribiendo 'ESC'."""
     bucle = True
 
     while bucle:
-        x = input("Añade un elemento a la lista o escribe '_esc_' para salir")
-        if x == "_esc_":
+        x = input("Añade un elemento a la lista o escribe 'ESC' para salir: ")
+        if x == "ESC":
             bucle = False
         else:
             lista.append(x)
@@ -81,7 +77,6 @@ def elige(lista, mensaje):
     Se le pide al usuario que escriba su opción escogida <mensaje>
     y si está en la <lista> imprime correcto. Si no, repite.
     """
-
     userInput = input(mensaje).lower()
 
     while userInput not in lista:
@@ -101,7 +96,6 @@ def elige(lista, mensaje):
 
 def indexElem(lista):
     """Muestra los índices y elementos de una lista."""
-
     contador = 0
     for i in lista:
         print(f"[{contador}] = {i}")
@@ -110,19 +104,21 @@ def indexElem(lista):
 
 def elemIndex(lista):
     """Muestra los elementos de una lista y sus índices."""
-
     for i in range(len(lista)):
         print(f"{lista[i]} = [{i}]")
 
 
 def cuantoshay(lista, buscar):
     """Con una lista dada, encuentra las veces que aparece un elemento"""
-
+    # También se puede hacer con re.findall:
+    # coincidencias = re.findall(buscar, str(lista))
+    # print("Número de coincidencias:", len(coincidencias))
     numveces = 0
     for i in lista:
         if(i == buscar):
             numveces += 1
-    print("Número de coincidencias:", numveces)
+    # print("Número de coincidencias:", numveces)
+    return numveces
 
 
 def generaNums():
@@ -130,7 +126,6 @@ def generaNums():
     Se da un límite para generar números aleatorios
     y crea una lista con ellos tan extensa como se especifique.
     """
-
     cont = 1
     miLista = []
     limite = int(input("Rango de 1 a"))
@@ -140,3 +135,9 @@ def generaNums():
         miLista.append(randint(1, limite))
         cont = cont+1
     return miLista
+
+
+def cartesian(list1, list2):
+    """Dadas 2 listas, devuelve las permutaciones posibles entre elementos."""
+    cartesList = [[a, b] for a in list1 for b in list2]
+    return cartesList
